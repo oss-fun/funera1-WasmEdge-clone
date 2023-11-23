534e63b87063132dffa79ba7e16fc7910e11fd64
funera1 <60760935+funera1@users.noreply.github.com>
2023-11-23 17:23:20 +0900

    Clean debug print

--- include/executor/migrator.h
+++ include/executor/migrator.h
@@ -594,7 +594,6 @@ public:
   }
   
   Expect<std::vector<Runtime::StackManager::Frame>> restoreStackMgrFrame() {
-    std::cout << "Enter restoreStackMgrFrame" << std::endl;
     std::ifstream FrameStream;
     FrameStream.open("stackmgr_frame.img");
     Runtime::StackManager StackMgr;
@@ -604,12 +603,10 @@ public:
     std::string FrameString;
     /// TODO: ループ条件見直す
     std::map<std::string, const Runtime::Instance::ModuleInstance*> ModCache;
-    std::cout << "restoreStackMgrFrame 1" << std::endl;
     while(getline(FrameStream, FrameString)) {
       // ModuleInstance
       std::string ModName = FrameString;
       const Runtime::Instance::ModuleInstance* ModInst;
-      // std::cout << "restore frame: 1" << std::endl;
 
       // ModInstがnullの場合
       if (ModName == NULL_MOD_NAME) {
@@ -625,31 +622,19 @@ public:
 
       // ModInstがnullじゃない場合
       ModInst = findModule(ModName);
-      // std::cout << "restored ModName is " << ModName << std::endl;
       if (ModInst == nullptr) {
-        // std::cout << "ModInst is nullptr" << std::endl;
         assert(-1);
       }
-      // std::cout << "restore frame: 2" << std::endl;
 
-      std::cout << "restoreStackMgrFrame 2" << std::endl;
-      std::cout << "ModCache.count(ModName) is " << ModCache.count(ModName) << std::endl;
       /// TODO: 同じModuleの復元をしないよう、キャッシュを作る
       if (ModCache.count(ModName) == 0) {
-        std::cout << "before restore memory" << std::endl;
         ModInst->restoreMemInst(std::string(ModName));
-        std::cout << "restore memory" << std::endl;
-        // std::cout << "Success restore meminst" << std::endl;
         ModInst->restoreGlobInst(std::string(ModName));
-        std::cout << "restore global" << std::endl;
-        // std::cout << "Success restore globinst" << std::endl;
         ModCache[ModName] = ModInst;
       }
       else {
         ModInst = ModCache[ModName];
       }
-      std::cout << "restoreStackMgrFrame 3" << std::endl;
-      // std::cout << "restore frame: 3" << std::endl;
 
       // Iterator
       getline(FrameStream, FrameString);
@@ -661,9 +646,7 @@ public:
         return Unexpect(Res);
       }
       AST::InstrView::iterator From = Res.value();
-      std::cout << "restoreStackMgrFrame 4" << std::endl;
       // AST::InstrView::iterator From = _restoreIter(ModInst, FuncIdx, Offset).value();
-      // std::cout << "restore frame: 4" << std::endl;
 
       // Locals, VPos, Arity
       getline(FrameStream, FrameString);
@@ -672,7 +655,6 @@ public:
       uint32_t VPos = static_cast<uint32_t>(std::stoul(FrameString));
       getline(FrameStream, FrameString);
       uint32_t Arity = static_cast<uint32_t>(std::stoul(FrameString));
-      std::cout << "restoreStackMgrFrame 5" << std::endl;
 
       Runtime::StackManager::Frame f(ModInst, From, Locals, Arity, VPos);
       FrameStack.push_back(f);
@@ -680,7 +662,6 @@ public:
       // 空の行を読み捨て
       getline(FrameStream, FrameString);
     }
-    std::cout << "Finish restoreStackMgrFrame" << std::endl;
 
     FrameStream.close();
     return FrameStack;
